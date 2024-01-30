c0102b1fa29ecbc5228b91d0e975a6290f638c96
Andreas691667 <andreaskaag@hotmail.com>
2024-01-30 11:51:20 +0100

    increased sleep

--- rtde/UR_RTDE_CLIENT_LIBRARY/examples/simple_control.py
+++ rtde/UR_RTDE_CLIENT_LIBRARY/examples/simple_control.py
@@ -11,9 +11,9 @@ con.connect((ROBOT_HOST, ROBOT_PORT))
 # con.send(b"set_digital_out(2,True)" + b"\n")
 while True:
     con.send(b"movej([-1.94, -1.58, 1.16, -1.15, -1.55, 1.18], a=1.0, v=0.6)" + b"\n")
-    time.sleep(5)
+    time.sleep(15)
     con.send(b"movej([1.94, -1.58, 1.16, -1.15, -1.55, 1.18], a=1.0, v=0.6)" + b"\n")
-    time.sleep(5)
+    time.sleep(15)
 
 
 data = con.recv(1024)
