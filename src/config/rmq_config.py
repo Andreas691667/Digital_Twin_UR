f940d362fad91004e53a3aa8fc26fa4686c9a224
Andreas691667 <andreaskaag@hotmail.com>
2024-02-06 10:47:40 +0100

    updated ip in rmq config

--- src/config/rmq_config.py
+++ src/config/rmq_config.py
@@ -1,6 +1,6 @@
 class RMQ_CONFIG():
     """RabbitMQ configuration"""
-    RMQ_SERVER_IP = "localhost"
+    RMQ_SERVER_IP = "192.168.0.165"
     RMQ_SERVER_PORT = 5672
     MONITOR_EXCHANGE = "MONITOR_EXCHANGE"
     DT_EXCHANGE = "DT_EXCHANGE"
