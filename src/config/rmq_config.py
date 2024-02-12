775377c0da9f2d682a880decf7884c941f89b66a
Andreas691667 <andreaskaag@hotmail.com>
2024-02-12 16:51:09 +0100

    Update RMQ and robot IP addresses

--- src/config/rmq_config.py
+++ src/config/rmq_config.py
@@ -1,6 +1,6 @@
 class RMQ_CONFIG():
     """RabbitMQ configuration"""
-    RMQ_SERVER_IP = "192.168.0.165"
+    RMQ_SERVER_IP = "10.0.0.228" #Andreas home:  10.0.0.228, Andreas drobotti: 192.168.0.165
     RMQ_SERVER_PORT = 5672
     MONITOR_EXCHANGE = "MONITOR_EXCHANGE"
     DT_EXCHANGE = "DT_EXCHANGE"
