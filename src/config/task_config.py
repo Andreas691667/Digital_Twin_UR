83ca9c0d379c40a23fd878831b86a1f4bcb0043b
Andreas691667 <andreaskaag@hotmail.com>
2024-02-16 08:18:43 +0100

    Refactor task_config.py to use a dictionary for block_config

--- src/config/task_config.py
+++ src/config/task_config.py
@@ -1,19 +1,37 @@
 class TASK_CONFIG:
     TARGET_POSE = "TARGET_POSE"
     JOINT_POSITIONS = "JOINT_POSITIONS"
-    STOP = "STOP" # Decelerates to zero with deceleration given by argument
+    STOP = "STOP"  # Decelerates to zero with deceleration given by argument
     ARG_TYPES = [TARGET_POSE, JOINT_POSITIONS, STOP]
 
-    ur_script_mappings = {
-        TARGET_POSE: "movel",
-        JOINT_POSITIONS: "movej",
-        STOP: "stopj"
-    }
+    ur_script_mappings = {TARGET_POSE: "movel", JOINT_POSITIONS: "movej", STOP: "stopj"}
 
     # matrix of 4 waypoints and grip percentage
-    block_config = [[0.5, 0.5, 0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5, 0.5, 0.5],
-        41
-    ]
+    WAYPOINTS = "waypoints"
+    GRIP_PERCENTAGE = "grip_percentage"
+
+    """Config for the task.
+    To access waypoints, use block_config[block_number][WAYPOINTS],
+    where block_number is the block number.
+    To access grip percentage, use block_config[block_number][GRIP_PERCENTAGE],
+    where block_number is the block number."""
+    block_config = {
+        1 : {
+            WAYPOINTS: [
+                [0.5, 0.5, 0.5, 0.5],
+                [0.5, 0.5, 0.5, 0.5],
+                [0.5, 0.5, 0.5, 0.5],
+                [0.5, 0.5, 0.5, 0.5],
+            ],
+            GRIP_PERCENTAGE: 41,
+        },
+        2 : {
+            WAYPOINTS: [
+                [0.5, 0.5, 0.5, 0.5],
+                [0.5, 0.5, 0.5, 0.5],
+                [0.5, 0.5, 0.5, 0.5],
+                [0.5, 0.5, 0.5, 0.5],
+            ],
+            GRIP_PERCENTAGE: 41,
+        },
+    }
