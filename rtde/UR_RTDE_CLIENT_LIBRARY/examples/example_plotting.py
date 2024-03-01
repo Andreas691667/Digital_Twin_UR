6b0d5bb1b016b8ffdd2be9efe7eb250c4b6d8c8e
Andreas691667 <andreaskaag@hotmail.com>
2024-03-01 08:10:26 +0100

    test

--- rtde/UR_RTDE_CLIENT_LIBRARY/examples/example_plotting.py
+++ rtde/UR_RTDE_CLIENT_LIBRARY/examples/example_plotting.py
@@ -22,7 +22,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt 
 
 # import sys
 
