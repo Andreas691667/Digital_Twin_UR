6543c2600fc2a208b9ab244950de1f903a6e451c
BusterSalomon <bustersalomonrasmussen@gmail.com>
2024-03-07 11:54:11 +0100

    thresholds implemented in testing environment, not sure it works yet

--- src/ur3e/ur3e_tester.py
+++ src/ur3e/ur3e_tester.py
@@ -7,7 +7,12 @@ from config.task_config import TASK_CONFIG
 
 
 def compute_ik_solutions (task_config, robot_model) -> np.ndarray:
-    """Computes the inverse kinematics solutions"""
+    """Computes the inverse kinematics solutions
+        returns 3D matrix:
+            row: block number
+            column: the four positions for a task
+            depth: solutions for each position
+    """
     number_of_blocks: int = task_config[TASK_CONFIG.NO_BLOCKS] 
     solutions: np.ndarray = np.zeros(shape=(number_of_blocks, 4, 6))
     print(solutions.shape)
@@ -29,50 +34,87 @@ def compute_ik_solutions (task_config, robot_model) -> np.ndarray:
 
     return solutions
 
-def compute_thresholds (ik_solutions: np.ndarray):
+def get_distances_of_leading_axis(joint_positions: np.ndarray) -> np.ndarray:
+    # sliding window differences
+    joint_positions_differences = np.abs(joint_positions[:-1, :] - joint_positions[1:, :])
+    leading_axis = np.argmax(joint_positions_differences, axis=1)
+    differences_leading_axis = joint_positions_differences[np.arange(len(joint_positions_differences)), leading_axis]
+    return differences_leading_axis
+
+def get_durations_of_leading_axis(differences_leading_axis: np.ndarray) -> np.ndarray:
+    """Calculates the durations of the leading axis's
+        Note: This may be calculated differently for shorter movement, i.e. if it does not reach max speed
+    """
+    JOINT_SPEED_MAX = 60 # deg/s
+    JOINT_ACCELERATION = 80 # deg/s^2
+    ACCELERATION_TIME = JOINT_SPEED_MAX / JOINT_ACCELERATION
+    ACCELERATION_DIST = 1/2 * JOINT_ACCELERATION * ACCELERATION_TIME**2
+    
+    # duration of acceleration and deceleration of trapezoid
+    duration_of_acc = 2 * ACCELERATION_TIME
+    # duration of constant speed of trapezoid
+    duration_of_constant_speed = ((differences_leading_axis-ACCELERATION_DIST) / JOINT_SPEED_MAX)
+    # combined duration
+    combined_time_estimation_task = duration_of_constant_speed + duration_of_acc
+    return combined_time_estimation_task
+
+
+def get_duration_between_positions (joint_positions: np.ndarray) -> np.ndarray:
+    # Get the distances of the leading axis
+    distances_of_leading_axis = get_distances_of_leading_axis(joint_positions)
+
+    # Get the durations of movements
+    durations_of_leading_axis = get_durations_of_leading_axis(distances_of_leading_axis) 
+
+    # Sum the durations
+    combined_duration = np.sum(durations_of_leading_axis)
+
+    return combined_duration
+
+def get_home_and_origin(ik_solutions, robot_model):
+    HOME_X = TASK_CONFIG.HOME_POSITION[TASK_CONFIG.x]
+    HOME_Y = TASK_CONFIG.HOME_POSITION[TASK_CONFIG.y]
+    home_sol = robot_model.compute_joint_positions_xy(HOME_X, HOME_Y)
+    origin_task0 = ik_solutions[0, 0, :]
+    positions = np.vstack((home_sol, origin_task0))
+    return positions
+
+def compute_thresholds (ik_solutions: np.ndarray, robot_model):
     """Computes the thresholds corresponding to block movements
     see __compute_ik_solutions for input format
     """
-    # Based on largest distance between joint positions
-    JOINT_SPEED_MAX = 60 # rad/s
-    JOINT_ACCELERATION = 80 # rad/s^2
-    RISE_TIME = JOINT_SPEED_MAX / JOINT_ACCELERATION
-
     number_of_blocks, _, _ = np.shape(ik_solutions)
+
+    # Initialize threshold with time it takes to go from home to next block
+    home_and_origin = get_home_and_origin(ik_solutions, robot_model)
+    thresholds = [get_duration_between_positions(home_and_origin)]
     
     for block_number in range(number_of_blocks-1):
         joint_positions_current_block = ik_solutions[block_number, :, :]
         joint_positions_next_block = ik_solutions[block_number+1, :, :]
-        joint_positions_difference = np.abs(joint_positions_current_block-joint_positions_next_block)
-        leading_axis = np.argmax(joint_positions_difference, axis=1)
-
-        print(joint_positions_difference)
-        print(leading_axis)
-
-        # use leading axis of movement to calculate timing for each subtask in block
-        combined_time_estimation_for_block = 0 # initialize to 0
-        for subtask in range(4):
-            subtask_leading_axis = leading_axis[subtask]
-            radians_of_leading_axis = joint_positions_difference[subtask, subtask_leading_axis]
-            print(f"radians subtask {subtask} of block {block_number} \n", radians_of_leading_axis)
-            
-            # calculate distance covered in rise time
-            subtask_time = -1
-            # calculate time for movement based on speed and acceleration
-
-        # add combined to list
         
-    return []
+        # Get positions which matters for the timing calculations
+        # That is: All subtasks jp of the first task, and the first subtask jp of the next task
+        before_grip_joint_position = joint_positions_current_block[-2, :]
+        first_joint_position_of_next_block = joint_positions_next_block[0, :]
+        timing_essential_positions = np.vstack((joint_positions_current_block, before_grip_joint_position, first_joint_position_of_next_block))
+        
+        # Get durations in between positions
+        duration = get_duration_between_positions(timing_essential_positions)
+        thresholds.append(duration)
+        
+        
+    return thresholds
 
 if __name__ == "__main__":
     task_config = TASK_CONFIG.block_config_heart
     robot_model = UR3e()
 
-    print(TASK_CONFIG.RISE_TIME)
-    print(TASK_CONFIG.RISE_DIST)
-    # sol = compute_ik_solutions(task_config, robot_model)
-    # thresholds = compute_thresholds(sol)
-
+    sol = compute_ik_solutions(task_config, robot_model)
+    thresholds = compute_thresholds(sol, robot_model)
+    
+    print(sol)
+    print(thresholds)
     # print(sol)
     # block_number = 0
 
