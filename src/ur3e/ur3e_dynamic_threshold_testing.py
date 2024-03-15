58929700bb17987e89704317d84330a284e26811
BusterSalomon <bustersalomonrasmussen@gmail.com>
2024-03-15 14:08:53 +0100

    dynamic thresholds seems to be working

--- src/ur3e/ur3e_dynamic_threshold_testing.py
+++ src/ur3e/ur3e_dynamic_threshold_testing.py
@@ -50,29 +50,38 @@ def get_durations_of_leading_axis(distances_leading_axis: np.ndarray) -> np.ndar
     Output: Time it takes to traverse those distances
     Note: This may be calculated differently for shorter movement, i.e. if it does not reach max speed
     """
-    JOINT_SPEED_MAX = 60 # deg/s
-    JOINT_ACCELERATION = 80 # deg/s^2
-    ACCELERATION_TIME = JOINT_SPEED_MAX / JOINT_ACCELERATION # Time it takes to reach maximun velocity
-    ACCELERATION_DIST = 1/2 * JOINT_ACCELERATION * ACCELERATION_TIME**2
-    print(f"Acceleration distance: {ACCELERATION_DIST}")
+    JOINT_SPEED_MAX_DEG = 60 # deg/s
+    JOINT_ACCELERATION_DEG = 80 # deg/s^2
+    GRAP_TIME = 0.7 # s
+    JOINT_SPEED_MAX_RAD = np.deg2rad(JOINT_SPEED_MAX_DEG) # rad/s
+    JOINT_ACCELERATION_RAD = np.deg2rad(JOINT_ACCELERATION_DEG) # rad/s^2
+    ACCELERATION_TIME = JOINT_SPEED_MAX_RAD / JOINT_ACCELERATION_RAD # Time it takes to reach maximun velocity
+    ACCELERATION_DIST = 1/2 * JOINT_ACCELERATION_RAD * ACCELERATION_TIME**2
     
     # For every distance the robot have to travel it does:
     # 1) Move with constant acceleration
     # 2) Move with with constant max speed
     # 3) Move with constant decceleration
     # If the movement is short in distance, it might skip step 2.
-    
-    # duration of acceleration and deceleration of trapezoid
-    duration_of_acc = 2 * ACCELERATION_TIME
-    # duration of constant speed of trapezoid
-    # TODO: This returns a negative number! should be fixed
-    duration_of_constant_speed = ((distances_leading_axis-ACCELERATION_DIST) / JOINT_SPEED_MAX)
-    print(f"distances: {distances_leading_axis}")
-    print(f"constant speed distance: {distances_leading_axis - ACCELERATION_DIST}")
-    
-    # combined duration
-    combined_time_estimation_task = duration_of_constant_speed + duration_of_acc
-    all_durations = [ACCELERATION_TIME, duration_of_constant_speed, ACCELERATION_TIME]
+
+    all_durations = []
+    for _, distance in enumerate(distances_leading_axis):
+        # Check if movement reaches max speed
+        # Does not reach max speed
+        if ACCELERATION_DIST >= distance/2: # If the acceleration distance is greater than half of the distance, then it does not reach max speed
+            # Calculate time to reach half-distance
+            # d = 1/2 * a * t^2 => t = sqrt{2d/a}
+            duration_half_distance = np.sqrt((2*(distance/2))/JOINT_ACCELERATION_RAD)
+            all_durations.extend([duration_half_distance, duration_half_distance])
+        
+        # Reaches max speed
+        else: 
+            duration_of_constant_speed = ((distance-ACCELERATION_DIST*2) / JOINT_SPEED_MAX_RAD)
+            all_durations.extend([ACCELERATION_TIME, duration_of_constant_speed, ACCELERATION_TIME])
+
+    # Calculate combined time
+    all_durations.append(GRAP_TIME)
+    combined_time_estimation_task = np.sum(all_durations)
     return combined_time_estimation_task, all_durations
 
 
@@ -100,69 +109,61 @@ def get_home_and_origin(ik_solutions, robot_model):
     positions = np.vstack((home_sol, origin_task0))
     return positions
 
+def get_home_ik_sol(robot_model):
+    HOME_X = TASK_CONFIG.HOME_POSITION[TASK_CONFIG.x]
+    HOME_Y = TASK_CONFIG.HOME_POSITION[TASK_CONFIG.y]
+    home_sol = robot_model.compute_joint_positions_xy(HOME_X, HOME_Y)
+    return home_sol
+
 def compute_thresholds (ik_solutions: np.ndarray, robot_model):
     """Computes the thresholds corresponding to block movements
     see __compute_ik_solutions for input format
     """
     number_of_blocks, _, _ = np.shape(ik_solutions)
-
-    # Initialize threshold with time it takes to go from home to next block
-    home_and_origin = get_home_and_origin(ik_solutions, robot_model)
-    combined_home_origin_duration, home_origin_durations  = get_duration_between_positions(home_and_origin)
-    thresholds = [combined_home_origin_duration]
-    all_durations = home_origin_durations
-    # print(f"Home -> Origin: {thresholds}")
+    
+    # Initialize threshold
+    thresholds = []
+    all_durations = []
+  
     
     # Calculate thresholds for blocks, if there is more than one block to move
-    for block_number in range(number_of_blocks-1):
-        joint_positions_current_block = ik_solutions[block_number, :, :]
-        joint_positions_next_block = ik_solutions[block_number+1, :, :]
-        
+    for block_number in range(number_of_blocks):
         # Get positions which matters for the timing calculations
         # That is: All subtasks jp of the first task, and the first subtask jp of the next task
-        before_grip_joint_position = joint_positions_current_block[-2, :] # The gripper goes up before it moves on
-        first_joint_position_of_next_block = joint_positions_next_block[0, :]
-        timing_essential_positions = np.vstack((joint_positions_current_block, before_grip_joint_position, first_joint_position_of_next_block))
+        timing_essential_positions = []
+        
+        # First block (or last if there are only one)
+        if block_number == 0:
+            home_sol = get_home_ik_sol(robot_model)
+            origin_positions = ik_solutions[block_number, :2, :]
+            timing_essential_positions = np.vstack((home_sol, origin_positions))
+        
+        # Middle block or last block
+        else:
+            joint_positions_current_block = ik_solutions[block_number-1, :, :]
+            joint_positions_next_block = ik_solutions[block_number, :, :]
+            before_grip_joint_position = joint_positions_current_block[-2, :] # The gripper goes up before it moves on
+            first_joint_positions_of_next_block = joint_positions_next_block[:2, :] # Origin before grip and grip pos
+            timing_essential_positions = np.vstack((joint_positions_current_block, before_grip_joint_position, first_joint_positions_of_next_block))
         
         # Get durations in between positions
         # combined_duration: total time between timing_essential_positions
         # durations: durations between all moves
         combined_duration, durations = get_duration_between_positions(timing_essential_positions)
         thresholds.append(combined_duration)
-        all_durations.append(durations)
+        all_durations.extend(durations)
         
         
     return thresholds, all_durations
 
 if __name__ == "__main__":
-    task_config = TASK_CONFIG.DYNAMIC_THRESHOLD_LONG_TASK
+    task_config = TASK_CONFIG.DYNAMIC_THRESHOLD_TWO_BLOCKS
     robot_model = UR3e()
 
     sol = compute_ik_solutions(task_config, robot_model)
-   
-    
-    # # Home, Origins, Targets
-    # positions = np.vstack((get_home_and_origin(sol, robot_model), sol[0, 1:, :]))
-    
-    # timing = get_duration_between_positions(positions)
-  
-    
     thresholds, all_durations = compute_thresholds(sol, robot_model)
+    
+    print(thresholds)
     print(all_durations)
-
-    # # print(sol)
-   
-    # print(sol)
-    # block_number = 0
-
-    # origin = task_config[block_number][TASK_CONFIG.ORIGIN]
-    # target = task_config[block_number][TASK_CONFIG.TARGET]
-
-    # # Calculate joint positions
-    # origin_q_start, origin_q, target_q_start, target_q = robot_model.compute_joint_positions_origin_target(
-    #     origin, target
-    # )
-
-    # print(type(origin_q_start))
 
     
