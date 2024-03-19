d4a4a8f803633b0a5f8ddd4452d6dfc88824c0ba
Andreas691667 <andreaskaag@hotmail.com>
2024-03-19 10:13:18 +0100

    proper shutdown by press of c

--- src/controller_monitor/controller_monitor_main.py
+++ src/controller_monitor/controller_monitor_main.py
@@ -1,13 +1,25 @@
 from ControllerMonitor import ControllerMonitor
 import argparse
+import msvcrt
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Controller Monitor')
-    parser.add_argument('-t', help='The name of the task to monitor')
+    parser.add_argument('-t', help='The name of the task to monitor', required=True)
     parser.add_argument('-home', type=bool, help='Go to home')
     args = parser.parse_args()
     task_name = args.t
     home = args.home
 
     cm = ControllerMonitor(task_name, home)
+
+    while True:
+        try:
+            k = msvcrt.getwche()
+            if k == "c":
+                break
+            k = "a"
+        except KeyboardInterrupt:
+            break
+
+    cm.shutdown_event.set()
