99c2282f54d7b67f1d3c05cc70f2595ca3694ac4
Andreas691667 <andreaskaag@hotmail.com>
2024-04-08 14:13:27 +0200

    removed legacy folder

--- legacy/controller/controller_main.py
+++
@@ -1,9 +0,0 @@
-from Controller import Controller
-
-if __name__ == "__main__":
-    controller = Controller()
-    controller.configure_rmq_client()
-    controller.robot_connect()
-
-    while True:
-        pass
--- legacy/monitor/monitor_main.py
+++
@@ -1,9 +0,0 @@
-from Monitor import Monitor
-import time
-
-if __name__ == "__main__":
-    monitor = Monitor()
-
-    time.sleep(50)
-
-    monitor.stop_recording()
