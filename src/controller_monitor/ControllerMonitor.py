04ff9cb61d380922b1b3bea8fb2b10d96edb5f8e
Andreas691667 <andreaskaag@hotmail.com>
2024-04-19 09:06:53 +0200

    clenaed a bit up

--- src/controller_monitor/ControllerMonitor.py
+++ src/controller_monitor/ControllerMonitor.py
@@ -1,5 +1,5 @@
 # from urinterface.robot_connection import RobotConnection
-from sys import path, exit
+from sys import path
 import numpy as np
 import ast
 from dataclasses import dataclass
@@ -19,22 +19,29 @@ import msvcrt  # for user input
 
 from rmq.RMQClient import Client
 from config.rmq_config import RMQ_CONFIG
-from config.msg_config import MSG_TYPES_DT_TO_CONTROLLER, MSG_TYPES_CONTROLLER_TO_DT, MSG_TYPES_MONITOR_TO_DT
+from config.msg_config import (
+    MSG_TYPES_DT_TO_CONTROLLER,
+    MSG_TYPES_CONTROLLER_TO_DT,
+    MSG_TYPES_MONITOR_TO_DT,
+)
 from config.grid_config import GRID_CONFIG
 from ur3e.ur3e import UR3e
 import json
 import yaml
 
+
 @dataclass
 class CMState:
     """Dataclass for controller monitor states"""
-    INITIALIZING = 0 # Before establishing connection to broker
-    WAITING_FOR_TASK_VALIDATION = 1 # Waiting for task validation
-    WAITING_FOR_USER_INPUT = 2 # Waits for user to press enter
-    NORMAL_OPERATION = 3 # Performing task
-    WAITING_FOR_FAULT_RESOLUTION = 4 # Waiting for resolution
-    MANUEL_INTERVENTION = 5 # Waiting for manuel intervention
-    SHUTTING_DOWN = 6 # Shutting down
+
+    INITIALIZING = 0  # Before establishing connection to broker
+    WAITING_FOR_TASK_VALIDATION = 1  # Waiting for task validation
+    WAITING_FOR_USER_INPUT = 2  # Waits for user to press enter
+    NORMAL_OPERATION = 3  # Performing task
+    WAITING_FOR_FAULT_RESOLUTION = 4  # Waiting for resolution
+    MANUEL_INTERVENTION = 5  # Waiting for manuel intervention
+    SHUTTING_DOWN = 6  # Shutting down
+
 
 class ControllerMonitor:
     """Class responsible for all robot interaction"""
@@ -48,13 +55,19 @@ class ControllerMonitor:
         #     GRID_CONFIG.block_config_close_blocks.copy()
         # )  # get own local copy of task config
 
-        self.dt_timer_finished : bool = False # flag to check if overall task is finished
-        self.task_finished : bool = False # flag to check if overall task is finished
-        self.task_validated : bool = False # flag to check if task is validated
+        self.dt_timer_finished: bool = (
+            False  # flag to check if overall task is finished
+        )
+        self.task_finished: bool = False  # flag to check if overall task is finished
+        self.task_validated: bool = False  # flag to check if task is validated
         self.task_config = None
         self.program_running_name: str = ""
         self.conf_file = "record_configuration.xml"
-        self.log_file = file_name_key + "_robot_output.csv" if file_name_key != "" else f"{task_name}_robot_output.csv"
+        self.log_file = (
+            file_name_key + "_robot_output.csv"
+            if file_name_key != ""
+            else f"{task_name}_robot_output.csv"
+        )
         self.log_file_path = Path("robot_output") / Path(self.log_file)
 
         # model of the robot
@@ -98,7 +111,7 @@ class ControllerMonitor:
                 rmq_client=self.rmq_client_out_monitor,
                 publish_topic_rmq=MSG_TYPES_MONITOR_TO_DT.MONITOR_DATA,
             )
-        ) 
+        )
 
         # load task
         self.load_task(task_name)
@@ -129,7 +142,7 @@ class ControllerMonitor:
                 print(f"\t [USER INPUT] {k}")
                 self.STATE = CMState.NORMAL_OPERATION
                 print("\t[STATE] NORMAL_OPERATION")
-            
+
             elif k == "i" and self.task_finished:
                 print(f"\t [USER INPUT] {k}")
                 self.invert_task()
@@ -149,15 +162,19 @@ class ControllerMonitor:
         # initialize task registers
 
         for i in range(self.task_config[GRID_CONFIG.NO_BLOCKS]):
-            self.task_config[i][GRID_CONFIG.ORIGIN], self.task_config[i][GRID_CONFIG.TARGET] = (
-            self.task_config[i][GRID_CONFIG.TARGET], self.task_config[i][GRID_CONFIG.ORIGIN])
+            (
+                self.task_config[i][GRID_CONFIG.ORIGIN],
+                self.task_config[i][GRID_CONFIG.TARGET],
+            ) = (
+                self.task_config[i][GRID_CONFIG.TARGET],
+                self.task_config[i][GRID_CONFIG.ORIGIN],
+            )
 
         self.block_number = 0
         self.task_finished = False
         # restart rtde connection
         self.rtde_connection = RTDEConnect(ROBOT_CONFIG.ROBOT_HOST, self.conf_file)
         self.STATE = CMState.INITIALIZING
-
 
     def init_robot_registers(self):
         """initialize the robot
@@ -170,7 +187,6 @@ class ControllerMonitor:
         sleep(0.01)
         self.load_program("/move_registers.urp")
         print("Robot initialization finished!")
-        
 
     def __initialize_task_registers(self, values) -> None:
         """initialize the task registers"""
@@ -180,7 +196,10 @@ class ControllerMonitor:
     def configure_rmq_clients(self):
         """configures rmq client to receive data from DT"""
         self.rmq_client_in.configure_incoming_channel(
-            self.on_rmq_message_cb, RMQ_CONFIG.DT_EXCHANGE, RMQ_CONFIG.FANOUT, RMQ_CONFIG.CONTROLLER_QUEUE
+            self.on_rmq_message_cb,
+            RMQ_CONFIG.DT_EXCHANGE,
+            RMQ_CONFIG.FANOUT,
+            RMQ_CONFIG.CONTROLLER_QUEUE,
         )
 
         self.rmq_client_out_monitor.configure_outgoing_channel(
@@ -209,7 +228,7 @@ class ControllerMonitor:
             data = json.loads(body)
             msg_type, msg_body = data.split(" ", 1)
             self.controller_queue.put((msg_type, msg_body))
-            print(f'\t [DT] {msg_type}')
+            print(f"\t [DT] {msg_type}")
         except ValueError:
             print("Invalid message format")
 
@@ -221,18 +240,12 @@ class ControllerMonitor:
 
     def load_program(self, program_name: str) -> None:
         """Load program"""
-        succ = self.robot_connection.load_program(program_name)
+        self.robot_connection.load_program(program_name)
         self.program_running_name = program_name
-        # print(f"Program '{program_name}' loaded: {succ}")
 
     def play_program(self, main_program=False) -> None:
         """Start loaded program"""
-        program_started = self.robot_connection.play_program()
-        # if program_started:
-        #     pass
-        #     # print(f"Program started: {program_started}")
-        # else:
-        #     pass
+        self.robot_connection.play_program()
 
         if main_program:
             sleep(1)
@@ -243,7 +256,7 @@ class ControllerMonitor:
         self.robot_connection.stop_program()
         print("Program stopped")
 
-    def __get_register_values (self) -> list:
+    def __get_register_values(self) -> list:
         """Gets the register values corresponding to the block to move"""
         # Get Origin and Target of current block
         origin = self.task_config[self.block_number][GRID_CONFIG.ORIGIN]
@@ -251,8 +264,8 @@ class ControllerMonitor:
 
         # Calculate joint positions
         # print(f"task config is: {self.task_config}")
-        origin_q_start, origin_q, target_q_start, target_q = self.robot_model.compute_joint_positions_origin_target(
-            origin, target
+        origin_q_start, origin_q, target_q_start, target_q = (
+            self.robot_model.compute_joint_positions_origin_target(origin, target)
         )
 
         # Check if any is nan
@@ -274,10 +287,10 @@ class ControllerMonitor:
         values = list(np.array(values).flatten())
 
         return values
-    
+
     def publish_task_to_DT(self) -> None:
         """Publish task to DT"""
-        msg = f'{MSG_TYPES_CONTROLLER_TO_DT.NEW_TASK} {self.task_config}'
+        msg = f"{MSG_TYPES_CONTROLLER_TO_DT.NEW_TASK} {self.task_config}"
         self.rmq_client_out_controller.send_message(msg, RMQ_CONFIG.CONTROLLER_EXCHANGE)
 
     def controller_worker(self):
@@ -307,7 +320,9 @@ class ControllerMonitor:
                 elif self.STATE == CMState.WAITING_FOR_TASK_VALIDATION:
                     # Wait for DT to validate task
                     if self.task_validated:
-                        print("\n \t [USER] Ready to play program. Press '2' to start, 'c' to exit \n")
+                        print(
+                            "\n \t [USER] Ready to play program. Press '2' to start, 'c' to exit \n"
+                        )
                         self.load_program("/move_registers.urp")
                         self.STATE = CMState.WAITING_FOR_USER_INPUT
                     else:
@@ -322,8 +337,10 @@ class ControllerMonitor:
                     if (not self.robot_connection.program_running()) and (
                         self.block_number < self.task_config[GRID_CONFIG.NO_BLOCKS]
                     ):
-                        print(f"Ready to take block number: {self.block_number} out of {self.task_config[GRID_CONFIG.NO_BLOCKS]-1} \n")
-                        
+                        print(
+                            f"Ready to take block number: {self.block_number} out of {self.task_config[GRID_CONFIG.NO_BLOCKS]-1} \n"
+                        )
+
                         # 1) get register values, by computing inverse kinematics
                         register_values = self.__get_register_values()
                         # print(f"Register values: {register_values}")
@@ -331,7 +348,7 @@ class ControllerMonitor:
                         # 2) initialize task registers
                         self.__initialize_task_registers(register_values)
 
-                        sleep(.5)
+                        sleep(0.5)
 
                         # 3) play the program
                         self.play_program(main_program=True)
@@ -340,14 +357,16 @@ class ControllerMonitor:
                         self.block_number += 1
 
                     # All blocks are done
-                    elif self.block_number == self.task_config[GRID_CONFIG.NO_BLOCKS] and (
-                        not self.robot_connection.program_running()
-                    ):
+                    elif self.block_number == self.task_config[
+                        GRID_CONFIG.NO_BLOCKS
+                    ] and (not self.robot_connection.program_running()):
                         self.__go_to_home()
                         sleep(1)
                         print("Task done")
                         self.init_robot_registers()
-                        print("\n \t [USER] Press 'i' to perform inverse task. Press 'c' to exit \n")
+                        print(
+                            "\n \t [USER] Press 'i' to perform inverse task. Press 'c' to exit \n"
+                        )
                         self.rtde_connection.shutdown()
                         self.STATE = CMState.WAITING_FOR_USER_INPUT
                         self.task_finished = True
@@ -385,21 +404,25 @@ class ControllerMonitor:
                 # DT could not resolve
                 elif msg_type == MSG_TYPES_DT_TO_CONTROLLER.COULD_NOT_RESOLVE:
                     print("DT could not resolve")
-                    self.robot_connection.popup("DT could not resolve. Task not possible. Exiting")
+                    self.robot_connection.popup(
+                        "DT could not resolve. Task not possible. Exiting"
+                    )
                     self.STATE = CMState.SHUTTING_DOWN
                     print("\t [STATE] SHUTTING_DOWN")
 
     def __go_to_home(self):
         """Go to home position"""
-        home_q = self.robot_model.compute_joint_positions_xy(GRID_CONFIG.HOME_POSITION[GRID_CONFIG.x],
-                                                          GRID_CONFIG.HOME_POSITION[GRID_CONFIG.y])
+        home_q = self.robot_model.compute_joint_positions_xy(
+            GRID_CONFIG.HOME_POSITION[GRID_CONFIG.x],
+            GRID_CONFIG.HOME_POSITION[GRID_CONFIG.y],
+        )
         self.robot_connection.movej(home_q)
 
-    def __reconfigure_task(self, new_task: str, decr:bool) -> None:
+    def __reconfigure_task(self, new_task: str, decr: bool) -> None:
         """function for reconfiguring PT task"""
         new_task_dict = ast.literal_eval(new_task)  # convert string to dict
         self.task_config = new_task_dict  # set new task
-        
+
         if decr:
             self.block_number -= 1
         else:
@@ -409,7 +432,7 @@ class ControllerMonitor:
         """shutdown everything: robot, rmq, threads"""
         while not self.shutdown_event.is_set():
             sleep(0.01)
-        
+
         print("Shutting down")
         self.stop_program()
         self.controller_thread_event.set()
